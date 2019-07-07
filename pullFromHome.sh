e86cc45a8da9798b3697ba4df55c598ac035403e
何立伟 <heliwei@megvii.com>
2019-07-07 11:20:20 +0800

    update push to home

--- pullFromHome.sh
+++ pullFromHome.sh
@@ -2,3 +2,7 @@
 cd $(dirname $0)
 cp ~/.vimrc.local ./DotFiles
 cp ~/.zshrc ./DotFiles
+pushd ~
+source .zshrc
+source .vimrc
+source .vimrc.local
--- pushToHome.sh
+++ pushToHome.sh
@@ -4,3 +4,6 @@ curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp ./DotFiles/.vimrc.local ~
 cp ./DotFiles/.zshrc ~
+source ~/.vimrc
+source ~/.vimrc.local
+source ~/.zshrc
